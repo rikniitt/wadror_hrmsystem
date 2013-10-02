b0dfa8c760cd0c7f6d8a8c78caa39b95012f49c2
rikniitt <riku.niittymaki@gmail.com>
2013-10-02 21:47:53 +0300

    fixed and deleted scaffolded specs

--- spec/models/faculty_spec.rb
+++ spec/models/faculty_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Faculty do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	it "is not saved without a name" do
+		faculty = Faculty.create description: "Dummy"
+
+		expect(faculty.valid?).to be(false)
+		expect(Faculty.count).to eq(0)
+	end
+
+	it "is not saved with too short name" do
+		faculty = Faculty.create name: "tooShort", description: "Dummy"
+
+		expect(faculty.valid?).to be(false)
+		expect(Faculty.count).to eq(0)
+	end
+	
+	
+	it "is is saved with valid name" do
+		faculty = Faculty.create name: "not Too Short", description: "Dummy"
+
+		expect(faculty.valid?).to be(true)
+		expect(Faculty.count).to eq(1)
+	end
 end
